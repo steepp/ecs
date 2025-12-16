ad4802f357011c447dd8ac8dc572b9b6e8c06101
hunt <72227379+steepp@users.noreply.github.com>
2025-12-16 13:01:28 +0100

    refactor component

--- src/component.js
+++ src/component.js
@@ -1,50 +1,53 @@
-export class Component {
+export class ComponentArray {
         #arr = [];
+
         addVal(val) {
                 this.#arr.push(val);
         }
+
         getVal(index) {
                 return this.#arr[index];
         }
+
         updateVal(index, val) {
                 this.#arr[index] = val;
         }
+
         #lastIndex() {
                 return this.#arr.length - 1;
         }
+
         removeVal(removeIndex) {
                 this.#arr[removeIndex] = this.#arr[this.#lastIndex()];
                 this.#arr.pop();
         }
 }
 
-export class ComponentMultiArray {
+export class ComponentMultiArray extends ComponentArray {
         constructor() {
+                super();
                 this.lastIndex = 0;
-                this.componentToIndex = {};
-                this.componentsMultiArray = [];
+                this.mapp = {};
+                this.multiarr = [];
         }
 
-        add(comp) {
-                this.componentToIndex[comp] = this.lastIndex;
-                this.componentsMultiArray.push(new Component());
+        add(componentName) {
+                this.mapp[componentName] = this.lastIndex;
+                super.addVal(new ComponentArray());
+
+                this.multiarr.push(componentName);
+
                 this.lastIndex++;
         }
 
-        getComponent(component) {
-                return this.componentsMultiArray[
-                        this.componentToIndex[component]
-                ];
+        updateComponentValueAtIndex(componentName, idx, val) {
+                const compOffset = this.mapp[componentName];
+                super.getVal(compOffset).updateVal(idx, val);
+                //this.multiarr[compOffset].updateVal(idx, val);
         }
 
-        updateComponent(id, comp, val) {
-                const idx = this.entityToIndex[id];
-                this.getComponent(comp).update(idx, val);
-        }
-
-        removeComponentValues(removeIndex) {
-                this.componentsMultiArray.forEach((c) =>
-                        c.removeValAt(removeIndex),
-                );
+        removeIndexValueInAllComponents(idx) {
+                this.multiarr.forEach((c) => c.removeVal(idx));
+                this.lastIndex--;
         }
 }
--- test/component.test.js
+++ test/component.test.js
@@ -1,20 +1,43 @@
 import { expect, it } from "vitest";
-import { Component } from "src/component.js";
+import { ComponentArray, ComponentMultiArray } from "src/component.js";
 
-const comp = new Component();
 
 it("component add value", () => {
+        const comp = new ComponentArray();
         comp.addVal(1);
         expect(comp.getVal(0)).toBe(1);
 });
 
+it("component add arr value", () => {
+        const comp = new ComponentArray();
+        comp.addVal([]);
+        expect(comp.getVal(0)).toStrictEqual([]);
+});
+
 it("component update value", () => {
+        const comp = new ComponentArray();
+        comp.addVal(77);
         comp.updateVal(0, 11);
         expect(comp.getVal(0)).toBe(11);
 });
 
 it("component remove value", () => {
+        const comp = new ComponentArray();
+
+        comp.addVal(112);
         comp.addVal(77);
+
         comp.removeVal(0);
+
+        expect(comp.getVal(0)).toEqual(77);
+});
+
+it.skip("componentmultiarray add value", () => {
+        const cmarr = new ComponentMultiArray();
+
+        cmarr.add()
+
+        cmarr.componentsMultiArray
+
         expect(comp.getVal(0)).toEqual(77);
 });
