e680892a26610251e3671a6079444bddb3e5f248
steepp <72227379+steepp@users.noreply.github.com>
2026-01-16 19:24:59 +0100

    junk

--- src/game.js
+++ src/game.js
@@ -32,54 +32,6 @@ export class SnapshotBuffer {
         }
 }
 
-export class SnapshotRepository extends Map {
-        constructor(...args) {
-                if (args && args > 0) {
-                        throw new Error("Use `set` method to add entries.");
-                }
-                super();
-        }
-
-        set(k, v) {
-                return super.set(k, v);
-        }
-
-        getFirstKey() {
-                return Array.from(this.keys())[0];
-        }
-
-        getLastKey() {
-                return last(Array.from(this.keys()));
-        }
-
-        getLatestSnapshot() {
-                const k = this.getLastKey();
-                return this.get(k);
-        }
-
-        /**
-         * Truncate the map to fixed SIZE items.
-         */
-        truncate() {
-                const limit = 15;
-                if (this.size / limit !== 1) {
-                        // TODO: keep only 15 items
-                }
-
-                const count = this.size() - limit;
-                if (count <= 0) {
-                        return false;
-                }
-                const delCount = count > 0 ? delCount : 0;
-
-                const keys = Array.from(this.keys());
-                const keysToDel = keys.splice(0, delCount);
-                // remove first delCount items
-                keysToDel.forEach((k) => this.delete(k));
-                return true;
-        }
-}
-
 let serverTime,
         clientTime = null;
 
@@ -98,13 +50,4 @@ export function updateClientServerTime(t) {
 
 export function getEstimatedServerTime() {
         return serverTime + (Date.now() - clientTime);
-}
-
-/**
- * This acts as a factory and cache for Entity flyweight objects.
- */
-export class EntityFactory {
-        static createEntity(id) {
-                return;
-        }
 }
