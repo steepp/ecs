816596bd7cd755a3779a7e9e04bc57b8090c116f
steepp <72227379+steepp@users.noreply.github.com>
2026-01-16 19:24:09 +0100

    read from circ buffer

--- src/main.js
+++ src/main.js
@@ -5,8 +5,7 @@ import { getControls } from "./controls.js";
 import {
         updateClientServerTime,
         getEstimatedServerTime,
-        SnapshotRepository,
-        EntityFactory,
+        SnapshotBuffer,
 } from "./game.js";
 
 let delta = 0;
@@ -14,18 +13,10 @@ let oldTime = 0;
 let requestId = null;
 let intervalID = null;
 
-const snapshots = new SnapshotRepository();
+const snapshots = new SnapshotBuffer(200);
 const network = new SocketNetwork();
 
-let matchingEntityIds = null;
-let matchingEntityPairsAttrs = null;
-
-const CL_INTERP = 0;
-
-const computeAlpha = (t, t0, t1) => (t - t0) / (t1 - t0);
-
-const findMatchingEntityId = (s1, s2) =>
-        s2.keys().filter((k) => k in s1.keys());
+let tmp;
 
 function mainLoop(currentTime) {
         fps.countFrames(currentTime);
@@ -33,42 +24,15 @@ function mainLoop(currentTime) {
         delta = (currentTime - oldTime) / 1000; // time in seconds
         oldTime = currentTime;
 
-        if (CL_INTERP) {
-                const DELAY = 100;
-                const t = getEstimatedServerTime() - DELAY;
-
-                const timestamps = Array.from(snapshots.keys()).sort();
-                const i = timestamps.findIndex((k) => k <= t);
-
-                const condition = i < 0 || (i == 0 && snapshots.size() == 1);
-                if (!condition) {
-                        const t0 = timestamps[i];
-                        const t1 = timestamps[i + 1];
-
-                        const alpha = computeAlpha(t, t0, t1);
-
-                        const snapshotStartEntities = snapshots.get(t0);
-                        const snapshotNextEntities = snapshots.get(t1);
-
-                        matchingEntityIds = findMatchingEntityId(
-                                snapshotNextEntities,
-                                snapshotStartEntities,
-                        );
-
-                        matchingEntityPairsAttrs = matchingEntityIds.forEach(
-                                (id) => {
-                                        const entityStart =
-                                                snapshotStartEntities[id];
-                                        const entityNext =
-                                                snapshotNextEntities[id];
-                                        return [entityStart, entityNext];
-                                },
-                        );
+        try {
+                if (snapshots.count > 0) {
+                        tmp = snapshots.read()["data"];
                 }
+        } catch (err) {
+                console.log(err);
         }
 
-        draw(snapshots.getLatestSnapshot(), delta);
-        //draw(delta);
+        draw(tmp || [], delta);
 
         requestId = requestAnimationFrame(mainLoop);
 }
@@ -85,21 +49,6 @@ function startAnimationFrame() {
 
 function stopAnimationFrame() {
         if (requestId) cancelAnimationFrame(requestId);
-}
-
-function saveSnapshot(snapshot) {
-        const t = snapshot.t;
-        let entities = snapshot.data;
-        snapshots.set(t, entities);
-
-        if (snapshots.size > 3) {
-                snapshots.truncate();
-        }
-}
-
-function inititLocalEntities(snapshot) {
-        const entities = snapshot.data;
-        entities.forEach(EntityFactory.createEntity);
 }
 
 /**
@@ -123,14 +72,13 @@ function startRenderLoop() {
 
         network.onMessage((snapshot) => {
                 updateClientServerTime(snapshot?.t || Date.now());
-                saveSnapshot(snapshot);
+                snapshots.write(snapshot);
         });
 
         const onConnect = (snapshot) => {
                 const serverTime = snapshot?.t || Date.now();
                 updateClientServerTime(serverTime);
-                saveSnapshot(snapshot);
-                inititLocalEntities(snapshot);
+                snapshots.write(snapshot);
                 startRenderLoop();
         };
         const nickName = "Romulus_" + new Date().toJSON();
