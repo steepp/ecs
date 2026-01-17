4bb27d291f3c0d95e404bd0694ab4d63823a2691
steepp <72227379+steepp@users.noreply.github.com>
2026-01-17 13:35:48 +0100

    clean up

--- src/main.js
+++ src/main.js
@@ -2,11 +2,7 @@ import { fps } from "./fps.js";
 import { draw } from "./render.js";
 import { SocketNetwork } from "./network.js";
 import { getControls } from "./controls.js";
-import {
-        updateClientServerTime,
-        getEstimatedServerTime,
-        SnapshotBuffer,
-} from "./game.js";
+import { updateClientServerTime, SnapshotBuffer } from "./game.js";
 
 let delta = 0;
 let oldTime = 0;
@@ -16,7 +12,7 @@ let intervalID = null;
 const snapshots = new SnapshotBuffer(200);
 const network = new SocketNetwork();
 
-let tmp;
+let tmp = [];
 
 function mainLoop(currentTime) {
         fps.countFrames(currentTime);
@@ -24,25 +20,15 @@ function mainLoop(currentTime) {
         delta = (currentTime - oldTime) / 1000; // time in seconds
         oldTime = currentTime;
 
-        try {
-                if (snapshots.count > 0) {
-                        tmp = snapshots.read()["data"];
-                }
-        } catch (err) {
-                console.log(err);
+        if (snapshots.count > 0) {
+                tmp = snapshots.read()["data"];
         }
 
-        draw(tmp || [], delta);
+        draw(tmp, delta);
 
         requestId = requestAnimationFrame(mainLoop);
 }
 
-/**
- * The frequency of calls to the callback function will generally
- * match the display refresh rate. `requestAnimationFrame()` calls
- * are paused in most browsers when running in background tabs or
- * hidden <iframe>s, in order to improve performance and battery life.
- */
 function startAnimationFrame() {
         requestId = requestAnimationFrame(mainLoop);
 }
@@ -51,9 +37,6 @@ function stopAnimationFrame() {
         if (requestId) cancelAnimationFrame(requestId);
 }
 
-/**
- * Start the game loop.
- */
 function startRenderLoop() {
         const delay = 1000 / 30;
         intervalID = setInterval(() => {
